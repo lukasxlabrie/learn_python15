4c9e59718b959fef2bc064234a14a68d2c89045b
misosucky <126988962+Misosucky@users.noreply.github.com>
2023-09-14 19:31:28 -0400

    added notes

--- ex15.py
+++ ex15.py
@@ -1,12 +1,17 @@
+#tells python tp save user input for later use
 from sys import argv
 
+#tells python that the input will be used for "filename"
 script, filename = argv
 
+#prints a meassge
 txt = open(filename)
 
+#calls a fucntion on txt called "read"
 print(f"Here's your file {filename}:")
 file_again = input("> ")
 
+#the rest of the srcipt is nore of the same
 print(txt.read())
 file_again =input("> ")
 
