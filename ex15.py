ecdaac3a04e51109604e707f4dbb0c3c36a96474
misosucky <126988962+Misosucky@users.noreply.github.com>
2023-09-14 19:46:01 -0400

    typo

--- ex15.py
+++ ex15.py
@@ -12,7 +12,7 @@ print(f"Here's your file {filename}:")
 file_again = input("> ")
 
 #the rest of the srcipt is nore of the same
-print(txt.read())
+print("Type The filename again:")
 file_again =input("> ")
 
 txt_again = open(file_again)
